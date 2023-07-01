d503f21607d927684f985f861966afba8f2b1d4e
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2023-06-30 23:38:35 -0500

    Shoot System Update

--- Assets/Weapons/Scripts/PistolBehavior.cs
+++ Assets/Weapons/Scripts/PistolBehavior.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PistolBehavior : WeaponBaseBehavior
 {
-    public override void Shoot()
+    protected override void Shoot()
     {
         base.Shoot();
     }
--- Assets/Weapons/Scripts/WeaponBaseBehavior.cs
+++ Assets/Weapons/Scripts/WeaponBaseBehavior.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class WeaponBaseBehavior : MonoBehaviour
 {
@@ -8,17 +9,16 @@ public class WeaponBaseBehavior : MonoBehaviour
     [SerializeField] protected int magSize;
     [SerializeField] protected float rateOfFire;
     [SerializeField] protected int bulletsPerShot;
-    //[SerializeField] protected Weapon_SO W_Ref;
-    [SerializeField] protected GameObject weaponDir;
+    [SerializeField] protected ShootConfigSC ShootConfig;
+    [SerializeField] protected TrailConfigSC TrailConfig;
     
     protected int bulletsLeft;
     protected bool _nextShotReady;
-    
 
-    Camera WeaponCam;
-    GameObject weaponProjectile;
-    Vector3 targetPoint;
-    Vector3 weaponSpread;
+    private ObjectPool <TrailRenderer> TrailPool;
+    private ObjectPool <GameObject> Bullet;
+    private float LastShootTime;
+    private ParticleSystem ShootSystem; 
     
 
 
@@ -26,8 +26,9 @@ public class WeaponBaseBehavior : MonoBehaviour
     protected virtual void Start()
     {
         bulletsLeft = magSize;
-        _nextShotReady = true;
-        WeaponCam = GameObject.Find("WeaponCamera").GetComponent<Camera>();
+        ShootSystem = GameObject.Find("MuzzleFlash01").GetComponent<ParticleSystem>();
+        TrailPool = new ObjectPool<TrailRenderer>(CreateTrail);
+        // WeaponCam = GameObject.Find("WeaponCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
@@ -37,70 +38,109 @@ public class WeaponBaseBehavior : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shoot();
-            RaycastCheck();
         }
         
     }
 
-    public virtual void Shoot()
+    protected virtual void Shoot()
     {
-            if(bulletsLeft != 0 && _nextShotReady == true)
-            {
-                ///Instantiate(W_Ref.ImpactEffect.GetComponent<ParticleSystem>(), Hit.transform.position, Hit.transform.rotation);
-                bulletsLeft--;
-                _nextShotReady = false;
-                Invoke("NextShot", rateOfFire);
-                Debug.Log(bulletsLeft);
-            }
-    }
-
-    public virtual void NextShot()
-    {
-        _nextShotReady = true;
-        Debug.Log("Next shot ready!");
-    }
-
-    public virtual void RaycastCheck()
-    {
-        RaycastHit Hit;
-
-        Ray ray = WeaponCam.ViewportPointToRay(new Vector3(0.5f,0.5f,0));
-
-        if (Physics.Raycast(ray, out Hit))
-            targetPoint = Hit.point;
-        else
-            targetPoint = WeaponCam.transform.position + WeaponCam.transform.forward * 75;
-
-        Vector3  dirWithoutSpread = targetPoint - weaponDir.transform.forward;
-
-        weaponSpread = dirWithoutSpread;
-        Debug.Log("Og Target = " + targetPoint);
-        Debug.Log("Weapon Dir = " + weaponDir.transform.forward);
-        Debug.Log("Dir no Spread = " + dirWithoutSpread);
-        Debug.Log("Weapon Spread = " + weaponSpread);
-
-        
-        CallBullet();
-        weaponProjectile.transform.forward = weaponSpread;
-        weaponProjectile.transform.parent = null;
-
-        Debug.Log("Bullet Direction = " +  weaponProjectile.transform.forward);
-
-        weaponProjectile.GetComponent<Rigidbody>().AddForce(weaponSpread * 200, ForceMode.Force);
-        //weaponProjectile.GetComponent<Rigidbody>().AddForce(weaponDir.transform.up * 0, ForceMode.Impulse);
-
-    }
-
-    public void CallBullet()
-    {
-        weaponProjectile = ObjectPooling.GiveObj(0);
-        if (weaponProjectile != null)
+        if(bulletsLeft != 0)
         {
-            weaponProjectile.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
-            weaponProjectile.SetActive(true);
+            if(Time.time > ShootConfig.FireRate + LastShootTime)
+            {
+                LastShootTime = Time.time;
+                ShootSystem.Play();
+                Vector3 shootDir = ShootSystem.transform.forward+ new Vector3(
+                    Random.Range(
+                        -ShootConfig.Spread.x,
+                        ShootConfig.Spread.x
+                    ), 
+                    Random.Range(
+                        -ShootConfig.Spread.y,
+                        ShootConfig.Spread.y
+                    ), 
+                    Random.Range(
+                        -ShootConfig.Spread.z,
+                        ShootConfig.Spread.z
+                    )
+                );
+
+                shootDir.Normalize();
+
+                if(Physics.Raycast(ShootSystem.transform.position,shootDir, out RaycastHit hit, float.MaxValue, ShootConfig.HitMask))
+                {
+                    StartCoroutine(
+                        PlayTrail(
+                            ShootSystem.transform.position,
+                            hit.point,
+                            hit
+                        )
+                    );
+                }
+                else
+                {
+                    StartCoroutine(
+                        PlayTrail(
+                            ShootSystem.transform.position,
+                            ShootSystem.transform.position + (shootDir * TrailConfig.MissDistance),
+                            new RaycastHit()
+                        )
+                    );
+                }
+            
+            }
+            bulletsLeft--;
+            Debug.Log(bulletsLeft);
+        }
+    }
+
+    protected TrailRenderer CreateTrail()
+    {
+        GameObject instance = new GameObject("Bullet Trail");
+        TrailRenderer trail = instance.AddComponent<TrailRenderer>();
+        trail.colorGradient = TrailConfig.Color;
+        trail.material = TrailConfig.Material;
+        trail.widthCurve = TrailConfig.WidthCurve;
+        trail.time = TrailConfig.Duration;
+        trail.minVertexDistance = TrailConfig.MinVertexDistance;
+        
+        trail.emitting = false;
+        trail.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+
+        return trail;
+    }
+
+    protected IEnumerator PlayTrail(Vector3 StartPoint, Vector3 EndPoint, RaycastHit Hit)
+    {
+        TrailRenderer instance = TrailPool.Get();
+        instance.gameObject.SetActive(true);
+        instance.transform.position = StartPoint;
+        yield return null; //Avoid position carry over
+
+        instance.emitting = true;
+
+        float distance = Vector3.Distance(StartPoint, EndPoint);
+        float remainingDistance = distance;
+        while(remainingDistance > 0)
+        {
+            instance.transform.position = Vector3.Lerp(
+                StartPoint,
+                EndPoint, 
+                Mathf.Clamp01(1 - (remainingDistance / distance))
+            );
+            remainingDistance -= TrailConfig.SimulationSpeed * Time.deltaTime;
+
+            yield return null;
         }
 
-        
+        instance.transform.position = EndPoint;
+
+        yield return new WaitForSeconds(TrailConfig.Duration);
+        yield return null;
+        instance.emitting = false;
+        instance.gameObject.SetActive(false);
+        TrailPool.Release(instance);
+
     }
 
 
