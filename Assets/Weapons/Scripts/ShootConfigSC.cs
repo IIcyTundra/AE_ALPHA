6bf7b6669c1fbcfaf69254cd8ee50a7eed6595e4
Jay Simmons <79724024+IIcyTundra@users.noreply.github.com>
2023-07-01 00:40:49 -0500

    Small edits

--- Assets/Weapons/Scripts/ShootConfigSC.cs
+++ Assets/Weapons/Scripts/ShootConfigSC.cs
@@ -8,4 +8,6 @@ public class ShootConfigSC : ScriptableObject
     public LayerMask HitMask;
     public Vector3 Spread = new Vector3(0.1f, 0.1f, 0.1f);
     public float FireRate = 0.25f;
+    public int bulletsPerShot;
+    public float timeBetweenShots;
 }
--- Assets/Weapons/Scripts/WeaponBaseBehavior.cs
+++ Assets/Weapons/Scripts/WeaponBaseBehavior.cs
@@ -7,13 +7,12 @@ public class WeaponBaseBehavior : MonoBehaviour
 {
 
     [SerializeField] protected int magSize;
-    [SerializeField] protected float rateOfFire;
-    [SerializeField] protected int bulletsPerShot;
     [SerializeField] protected ShootConfigSC ShootConfig;
     [SerializeField] protected TrailConfigSC TrailConfig;
     
     protected int bulletsLeft;
     protected bool _nextShotReady;
+    protected int bulletsShot;
 
     private ObjectPool <TrailRenderer> TrailPool;
     private ObjectPool <GameObject> Bullet;
@@ -34,22 +33,29 @@ public class WeaponBaseBehavior : MonoBehaviour
     // Update is called once per frame
     protected virtual void Update()
     {
-        
+        //Calls Shoot() when left clicking
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
+            bulletsShot = 0;
             Shoot();
+            
         }
         
     }
+
+    #region Weapon Function
 
     protected virtual void Shoot()
     {
         if(bulletsLeft != 0)
         {
+            //If the ingame time is greater than the fire rate, plus time, then we shoot.
             if(Time.time > ShootConfig.FireRate + LastShootTime)
             {
                 LastShootTime = Time.time;
+                //Calls the muzzle flash
                 ShootSystem.Play();
+                //Gets the shoot dir with spread
                 Vector3 shootDir = ShootSystem.transform.forward+ new Vector3(
                     Random.Range(
                         -ShootConfig.Spread.x,
@@ -67,35 +73,54 @@ public class WeaponBaseBehavior : MonoBehaviour
 
                 shootDir.Normalize();
 
+                //Shoots out a ray with and starts a coroutine for the trail renderer
                 if(Physics.Raycast(ShootSystem.transform.position,shootDir, out RaycastHit hit, float.MaxValue, ShootConfig.HitMask))
                 {
-                    StartCoroutine(
-                        PlayTrail(
-                            ShootSystem.transform.position,
-                            hit.point,
-                            hit
-                        )
-                    );
+                        StartCoroutine(
+                            PlayTrail(
+                                ShootSystem.transform.position,
+                                hit.point,
+                                hit
+                            )
+                        );
                 }
                 else
                 {
-                    StartCoroutine(
-                        PlayTrail(
-                            ShootSystem.transform.position,
-                            ShootSystem.transform.position + (shootDir * TrailConfig.MissDistance),
-                            new RaycastHit()
-                        )
-                    );
+
+                
+                        StartCoroutine(
+                            PlayTrail(
+                                ShootSystem.transform.position,
+                                ShootSystem.transform.position + (shootDir * TrailConfig.MissDistance),
+                                new RaycastHit()
+                            )
+                        );
                 }
+                
+               
             
             }
             bulletsLeft--;
+            bulletsShot++;
+             if (bulletsShot < ShootConfig.bulletsPerShot && bulletsLeft > 0)
+             {
+                Invoke("Shoot", ShootConfig.FireRate);
+             }
+                
             Debug.Log(bulletsLeft);
         }
     }
 
+    #endregion
+
+    #region Trail Creation
+
+    //Makes a Trail
+
     protected TrailRenderer CreateTrail()
     {
+        
+
         GameObject instance = new GameObject("Bullet Trail");
         TrailRenderer trail = instance.AddComponent<TrailRenderer>();
         trail.colorGradient = TrailConfig.Color;
@@ -110,8 +135,11 @@ public class WeaponBaseBehavior : MonoBehaviour
         return trail;
     }
 
+    //Creates an instance of a trail from the object pool, calcs the position, and where it's going
+
     protected IEnumerator PlayTrail(Vector3 StartPoint, Vector3 EndPoint, RaycastHit Hit)
     {
+        
         TrailRenderer instance = TrailPool.Get();
         instance.gameObject.SetActive(true);
         instance.transform.position = StartPoint;
@@ -143,5 +171,6 @@ public class WeaponBaseBehavior : MonoBehaviour
 
     }
 
+    #endregion
 
 }
